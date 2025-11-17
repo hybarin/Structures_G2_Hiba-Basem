f4bef276b5ac14670c91b7ee75d42d5ebfb83a1b
HEBA <hybarin20@gmail.com>
2025-11-17 22:56:00 +0300

    V1

--- src/Arrays/Array.java
+++ src/Arrays/Array.java
@@ -2,6 +2,12 @@ package Arrays;
 
 
 public class Array {
+
+    public void traversal(int[]number){
+        for(int i=0;i<number.length;i++)
+            System.out.println("Index["+i+"]="+number[i]);
+    }
+
     public void R_traversal(int[]a){
         for(int i=4;i>=0;i--)
             System.out.println("Index["+i+"]="+a[i]);
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,10 +1,25 @@
 package Arrays;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[]numbers={1,2,3,4,5};
-        Array arr=new Array();
-        arr.R_traversal(numbers);
+       // int[]numbers={1,2,3,4,5};
+      //  Array arr=new Array();
 
+       // arr.traversal(numbers);
+      //  arr.R_traversal(numbers);
+
+
+        // من المستخدم
+
+        Scanner scanner=new Scanner(System.in);
+        Array arr=new Array();
+        int []number=new int[5];
+        System.out.println("Enter "+number.length+" numbers");
+        for (int i=0; i<number.length; i++)
+            number[i]=scanner.nextInt();
+        arr.traversal(number);
     }
 }
