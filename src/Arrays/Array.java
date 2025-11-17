f87e4cdfa705ba982d042a77db270b4e2ee641b8
HEBA <hybarin20@gmail.com>
2025-11-17 22:18:36 +0300

    V1

---
+++ src/Arrays/Array.java
@@ -0,0 +1,9 @@
+package Arrays;
+
+
+public class Array {
+    public void R_traversal(int[]a){
+        for(int i=4;i>=0;i--)
+            System.out.println("Index["+i+"]="+a[i]);
+    }
+}
---
+++ src/Arrays/Main.java
@@ -0,0 +1,10 @@
+package Arrays;
+public class Main {
+    public static void main(String[] args) {
+
+        int[]numbers={1,2,3,4,5};
+        Array arr=new Array();
+        arr.R_traversal(numbers);
+
+    }
+}
