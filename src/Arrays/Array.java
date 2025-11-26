375f2fc47e24c35e55e06124ccb18e3749077d1b
HEBA <hybarin20@gmail.com>
2025-11-26 22:36:59 +0300

    v2

--- src/Arrays/Array.java
+++ src/Arrays/Array.java
@@ -3,13 +3,61 @@ package Arrays;
 
 public class Array {
 
-    public void traversal(int[]number){
+    //طباعه
+    public void traversal(int[] number){
         for(int i=0;i<number.length;i++)
             System.out.println("Index["+i+"]="+number[i]);
     }
 
     public void R_traversal(int[]a){
-        for(int i=4;i>=0;i--)
+        for(int i = a.length-1; i >= 0; i--)
             System.out.println("Index["+i+"]="+a[i]);
     }
+
+
+
+     //تعديل
+    public void update(int[] a, int index, int nvalue) {
+            if (index < 0 || index >= a.length)
+                System.out.println("Index not found");
+            else
+                a[index] = nvalue;
+    }
+
+
+      //حذف
+    public void delete(int[] a, int index) {
+            if (index < 0 || index >= a.length)
+                System.out.println("Index not found");
+            else
+                a[index] = 0;
+    }
+
+    public void shiftLeft_Delete(int[] a, int index) {
+        for (int i = index; i < a.length - 1; i++) {
+            a[i] = a[i + 1];
+        }
+        a[a.length - 1] = 6;
+    }
+
+
+    public void shiftRight_Delete(int[] a, int index) {
+        for(int i = index; i > 0; i--) {
+            a[i] = a[i - 1];
+        }
+        a[0] = a[1] - 1;
+    }
+
+
+    //بحث
+    public int LinearSearch(int []a, int svalue){
+        for (int i=0; i<a.length;i++)
+        {
+            if(svalue==a[i])
+                return i;
+        }
+        return -1;
+    }
+
 }
+
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,25 +1,49 @@
 package Arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-       // int[]numbers={1,2,3,4,5};
-      //  Array arr=new Array();
+        int[]numbers={1,2,3,4,5};
+        Array arr=new Array();
 
-       // arr.traversal(numbers);
-      //  arr.R_traversal(numbers);
+
+        //طباعه عاديه
+        arr.traversal(numbers);
+        arr.R_traversal(numbers);
 
 
         // من المستخدم
-
-        Scanner scanner=new Scanner(System.in);
-        Array arr=new Array();
-        int []number=new int[5];
-        System.out.println("Enter "+number.length+" numbers");
-        for (int i=0; i<number.length; i++)
-            number[i]=scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int[] number = new int[5];
+        System.out.println("Enter " + number.length + " numbers");
+        for (int i = 0; i < number.length; i++)
+            number[i] = scanner.nextInt();
         arr.traversal(number);
+
+
+        //تعديل
+        arr.update(numbers,2,7);
+
+
+        //حذف
+        arr.delete(numbers,2);
+        arr.shiftLeft_Delete(numbers,0);
+        arr.shiftRight_Delete(numbers, 0);
+
+
+        //بحث
+        int result=arr.LinearSearch(numbers,2);
+        if(result!=-1)
+            System.out.println("value found at index :"+result);
+        else
+            System.out.println("Value not found");
+
+
+        //داله الطباعه النهائيه
+        arr.traversal(numbers);
+
     }
 }
